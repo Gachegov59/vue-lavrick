73315f9af921febf613bdae164dba8aa93e50d00
alex.g <aleks.g@digital-spectr.ru>
2020-06-29 13:04:23 +0500

    dz2 | styles validate

--- src/components/dz2.vue
+++ src/components/dz2.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   div
     .progress
-      .progress-bar(role="progressbar" aria-valuenow="50" aria-valuemax="100" style="width: 0")
+      .progress-bar(role="progressbar" aria-valuenow="50" aria-valuemax="100"  )
 
     form(@submit.prevent="formSubmited = true")
 
       .form-group.mb-1(v-for="(item, i) in info" :key="i")
-        label {{item.name}} <i class="fas "></i>
+        label {{item.name}} <i class="fas" :class="classes[i]"></i>
           input( v-model="item.value"  v-on:change="checkValue(i)" )
 
       button.btn.btn-primary(disabled) Send Data
@@ -44,19 +44,30 @@
             pattern: /.+/
           }
         ],
-        classes: []
+        classes: [],
+        status: {
+          width: 20
+        }
       }
     },
     methods: {
        checkValue(i) {
-          console.log(this.info[i].pattern.test(this.info[i].value))
-         if ((this.info[i].value) !== '') {
-           this.classes.push((this.info[i].pattern.test(this.info[i].value)) ? "fa-exclamation-circle" : 'fa-smile' )
+          console.log('pattern test', this.info[i].pattern.test(this.info[i].value))
+          console.log('pattern', this.info[i].pattern)
+          console.log('value', this.info[i].value)
+          console.log('i', i) //todo: есди заполнять не с первого косяк, вносит стил в первый
+
+         if (((this.info[i].value) !== '') || (this.info[i].pattern.test(this.info[i].value))) {
+           this.classes.splice(i, 1, ((this.info[i].pattern.test(this.info[i].value)) ? "fa-check-circle color-green" : "fa-exclamation-circle color-red" ))
+           this.status.width +=  100 / this.info.length
+
          }
          else {
-           this.classes.splice(i, 1)
+           this.classes.splice(i, 1, '')
+           this.status.width -=  100 / this.info.length
          }
          console.log(this.classes)
+         console.log(this.status.width)
        }
     }
 
